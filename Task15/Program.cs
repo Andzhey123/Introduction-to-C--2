100b5fb5e912e8698df91ab71d35c32c3077f409
Andzhey123 <beginov.a@gmail.com>
2022-08-11 21:29:08 +0300

    Добавил решение по 15 задаче

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine ("Программа програма проверки на выходной день по цифре обозначающей день недели");
+
+Console.Write ("Введите целую цифру: ");
+
+string? numberString = Console.ReadLine ();
+
+int number =  int.Parse (numberString!);
+
+if (number == 6 || number == 7)
+    Console.Write ("Сегодня выходной день.");
+
+else if (number < 1 || number >7)
+{
+    Console.WriteLine ("Введенная цифра не обозначает день недели");
+}
+else 
+    Console.Write ("Сегодня будний день.");
