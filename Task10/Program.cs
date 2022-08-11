6c585198d642d51c996a7d633c4160e4235939b1
Andzhey123 <beginov.a@gmail.com>
2022-08-11 20:21:27 +0300

    Добавил решение по задаче 10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Программа вывода второй цифры из введенного трехзначного числа.");
+
+Console.Write ("Введите целое, трехзначное число: ");
+
+string? NumberString = Console.ReadLine();
+
+int number = int.Parse (NumberString!);
+
+int secondnumber = (number % 100) / 10;
+
+Console.WriteLine("Вторая цифра введенного вами числа: "+secondnumber);
