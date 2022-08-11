a34377ef00e5ae3f377e501264df10d2714e0773
Andzhey123 <beginov.a@gmail.com>
2022-08-11 21:13:00 +0300

    Убрал закоммиченный кусок кода

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,24 +1,4 @@
-﻿// Console.WriteLine ("Программа вывода третьей цифры из введенного числа.");
-
-// Console.Write ("Введите целое число: ");
-
-// string? NumberString = Console.ReadLine();
-
-// int number = int.Parse (NumberString!);
-
-// int thirdnumber = number % 10;
-
-// if (thirdnumber<100)
-//     Console.WriteLine("Третьей цифры нет");
-// else
-//     Console.WriteLine("Третья цифра введенного вами числа: "+thirdnumber);
-// Напишите программу, которая выводит третью цифру 
-// заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-
-Console.WriteLine ("Программа вывода третьей цифры из введенного числа.");
+﻿Console.WriteLine ("Программа вывода третьей цифры из введенного числа.");
 
 Console.Write ("Введите целое число: ");
 
@@ -28,7 +8,7 @@ int number =  int.Parse (numberString!);
 
 if (number < 0) 
     number = number * -1;
-    
+
 if (number > 999)
 {
     while (number > 999)
